af257f61f53ebc1ead5a50f1529c1687718af2f8
masterconi <adam.petrovich.novikov@gmail.com>
2024-02-22 14:42:25 +0200

    add code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,70 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+ 
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
+
+  
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        break;
+      case kDefaultAuto:
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+
+  @Override
+  public void testInit() {}
+
+
+  @Override
+  public void testPeriodic() {}
+
+
+  @Override
+  public void simulationInit() {}
+
+
+  @Override
+  public void simulationPeriodic() {}
+}
